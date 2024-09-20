4d9684eb6489f0f46e3db57c19ad7431a1c3d885
Sivabalan-19 <pmsiva.1906@gmail.com>
2024-09-20 16:45:13 +0530

    conflitname

--- src/component/input/Searchbar.jsx
+++ src/component/input/Searchbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import '../../styles/Search.css'
-function Searchbarwelcome({hello}) {
+function Sivabalan({hello}) {
   return (
     <div className='searchbox'>
       <div><CiSearch />sivabalan branch</div>
@@ -10,4 +10,4 @@ function Searchbarwelcome({hello}) {
   )
 }
 
-export default Searchbarwelcome;
+export default Sivabalan;
