6dcbdf1ccd12d51f919730c08bbd8f5daf122f2d
Sivabalan-19 <pmsiva.1906@gmail.com>
2024-09-20 12:33:23 +0530

    first A(siva)

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -4,16 +4,26 @@ import Iconbox from '../Iconbox/Iconbox'
 import { MdDashboard } from 'react-icons/md'
 
 function Sidebar() {
-  // const[name,setname] = useState('')
+  const [name, setName] = useState('dash');
 
-  // const style = {
-  //   backgroundcolor: selected == name ?'white' : ''
-  // }
+  const styles= (item) => ({
+    // backgroundColor: name === item ?'white' : 'transparent',
+    backgroundColor: name === item ?'white' : 'transparent',
+    color:  name === item ?'#07273d' : 'white'
+  })
+
+  const  setname = (item) =>
+  {
+    setName(item)
+    console.log(name)
+  }
+
+  console.log(name)
   return (
     <div className='sibar'>
         <div className='iconbox'>
-                <div className='iconboxcon'><Iconbox name = 'Dashboard' icon={<MdDashboard />}/></div>
-                <div className='iconboxcon'><Iconbox name = 'Sales' icon={<MdDashboard />} /></div>
+                <div className='iconboxcon' style={styles('dash')} onClick={() => setname('dash')}><Iconbox name = 'Dashboard' icon={<MdDashboard />}/></div>
+                <div className='iconboxcon' style={styles('item')} onClick={ () =>setname('item')}><Iconbox name = 'Sales' icon={<MdDashboard />} /></div>
                 <div className='iconboxcon'><Iconbox name = 'Order' icon={<MdDashboard />}/></div>
                 <div className='iconboxcon'><Iconbox name = 'Customer' icon={<MdDashboard />}/></div>
                 <div className='iconboxcon'><Iconbox name = 'Inventory' icon={<MdDashboard />}/></div>
