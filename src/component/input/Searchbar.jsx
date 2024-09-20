a336a0e0d07c3dc440006bf5c802cf7d53d613df
Sivabalan-19 <pmsiva.1906@gmail.com>
2024-09-20 16:46:04 +0530

    conflitname

--- src/component/input/Searchbar.jsx
+++ src/component/input/Searchbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import '../../styles/Search.css'
-function SearchInputconflit() {
+function Sivabalan() {
   return (
     <div className='searchbox'>
       <div><CiSearch />sivabalan branch</div>
@@ -10,4 +10,4 @@ function SearchInputconflit() {
   )
 }
 
-export default SearchInputconflit
+export default Sivabalan
